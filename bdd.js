cbd71c5a3050d4478fb0e51959e04dbd11fa4ddf
virtuallink777 <158222154+virtuallink777@users.noreply.github.com>
2024-01-30 16:24:38 -0500

    Create bdd.js

---
+++ bdd.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export  const
+         PRODUCTS = [
+        { category: "Frutas", price: "$1", stocked: true, name: "Manzana" },
+        { category: "Frutas", price: "$1", stocked: true, name: "Fruta del dragón" },
+        { category: "Frutas", price: "$2", stocked: false, name: "Maracuyá" },
+        { category: "Carnes", price: "$10", stocked: true, name: "Pollo" },
+        { category: "Granos", price: "$1", stocked: true, name: "Maiz Tierno" },
+        { category: "Verduras", price: "$2", stocked: true, name: "Espinaca" },
+        { category: "Verduras", price: "$4", stocked: false, name: "Calabaza" },
+        { category: "Verduras", price: "$1", stocked: true, name: "Guisantes" },
+        { category: "Carnes", price: "$10", stocked: true, name: "Cerdo" },
+        { category: "Granos", price: "$1", stocked: false, name: "Frijol" },
+         ]
